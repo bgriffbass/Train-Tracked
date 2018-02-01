d793eb7d97d83912cdc6e536977bb5124b385137
bgriffbass <36048157+bgriffbass@users.noreply.github.com>
2018-02-01 15:54:19 -0500

    working location of user

--- Train Tracked/ViewController.swift
+++ Train Tracked/ViewController.swift
@@ -10,27 +10,37 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let location = CLLocationCoordinate2DMake(33.9961, 81.0274)
         
-        let span = MKCoordinateSpanMake(0.2, 0.2)
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
         
-        let dropPin = MKPointAnnotation()
-        dropPin.coordinate = location
-        dropPin.title = "You"
-        mapView.addAnnotation(dropPin)
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        let center = location.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(region, animated: true)
+        mapView.showsUserLocation = true
+        
+        
+        
+        
+        
     }
 
 
